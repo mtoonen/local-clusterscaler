439f0693c058038a1ca004880844f7b386474701
Meine Toonen <meine.toonen@malmberg.nl>
2023-04-12 14:44:52 +0200

    Working on cluster

--- operator/src/main/java/nl/meine/ClientProvider.java
+++ operator/src/main/java/nl/meine/ClientProvider.java
@@ -2,17 +2,17 @@ package nl.meine;
 
 
 import io.fabric8.kubernetes.api.model.KubernetesResourceList;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import nl.meine.models.LocalNode;
 
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.inject.Singleton;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -21,7 +21,7 @@ public class ClientProvider {
     @Produces
     @Singleton
     @Named("namespace")
-    String findNamespace() throws IOException {
+    String findNamespace() {
         try {
             return new String(Files.readAllBytes(Paths.get("/var/run/secrets/kubernetes.io/serviceaccount/namespace")));
         } catch (Exception e) {
@@ -31,8 +31,15 @@ public class ClientProvider {
 
     @Produces
     @Singleton
+    @Named("scalerEndpoint")
+    String getEndpointIP(LocalNodeResourceCache cache) {
+        return cache.get("hades").getSpec().getIpAddress() + ":8080";
+    }
+
+    @Produces
+    @Singleton
     @Named("mediaNamespace")
-    String findMediaNamespace() throws IOException {
+    String findMediaNamespace()  {
         return "media";
     }
 
@@ -40,26 +47,21 @@ public class ClientProvider {
     @Singleton
     @Named("nodewatcher")
     KubernetesClient newClient(@Named("namespace") String namespace) {
-/*
         Config config = new ConfigBuilder().withNamespace(namespace).build();
-        return new KubernetesClientBuilder().withConfig(config).build();*/
-        return new DefaultKubernetesClient().inNamespace(namespace);
+        return new KubernetesClientBuilder().withConfig(config).build();
     }
 
     @Produces
     @Singleton
     @Named("podClient")
     KubernetesClient podClient(@Named("mediaNamespace") String namespace) {
-/*
         Config config = new ConfigBuilder().withNamespace(namespace).build();
-        return new KubernetesClientBuilder().withConfig(config).build();*/
-        return new DefaultKubernetesClient().inNamespace(namespace);
+        return new KubernetesClientBuilder().withConfig(config).build();
     }
 
     @Produces
     @Singleton
-    MixedOperation<LocalNode, KubernetesResourceList<LocalNode>, Resource<LocalNode>> provideLocalNodeClient(@Named("nodewatcher")KubernetesClient defaultClient) {
-        MixedOperation<LocalNode, KubernetesResourceList<LocalNode>, Resource<LocalNode>> cronTabClient = defaultClient.resources(LocalNode.class);
-        return cronTabClient;
+    MixedOperation<LocalNode, KubernetesResourceList<LocalNode>, Resource<LocalNode>> provideLocalNodeClient(@Named("nodewatcher") KubernetesClient defaultClient) {
+        return defaultClient.resources(LocalNode.class);
     }
 }
--- operator/src/main/java/nl/meine/ScalerClient.java
+++ operator/src/main/java/nl/meine/ScalerClient.java
@@ -1,39 +1,37 @@
 package nl.meine;
 
-import io.netty.handler.codec.http.HttpMethod;
 import nl.meine.models.LocalNode;
 import nl.meine.models.LocalNodeSpec;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.net.http.HttpHeaders;
-import java.util.Collections;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 @ApplicationScoped
 public class ScalerClient {
+    private final static Log log = LogFactory.getLog(ScalerClient.class);
 
     @Inject
-    private WakeOnLan waker;
+    NodeTerminator arnold;
 
     @Inject
-    private NodeTerminator arnold;
+    LocalNodeResourceCache cache;
 
     @Inject
-    private LocalNodeResourceCache cache;
-
-    public  String endpoint = "192.168.68.117:8080";
+    @Named("scalerEndpoint")
+    String endpoint;
 
     public static void main(String[] args) {
         String mac = "7C:10:C9:B8:32:C5";
@@ -46,36 +44,27 @@ public class ScalerClient {
         sc.up(ln);
     }
 
-    public String up(LocalNode ln){
-        URL url = null;
+    public void up(LocalNode ln) {
+        URL url;
         try {
-//
-//            Map<String, String> parameters = new HashMap<>();
-//            parameters.put("macAddress", ln.getSpec().getMacAddress());
 
-            waker.wake(ln.getSpec().getMacAddress());
-            cache.setLocalNodeRunning(ln, true);
-            /*
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("macAddress", ln.getSpec().getMacAddress());
+
             String params = ParameterStringBuilder.getParamsString(parameters);
-            url = new URL("http://" + endpoint + "/scaler/up?"+params);
-            System.out.println(url.toString());
+            url = new URL("http://" + endpoint + "/scaler/up?" + params);
 
             StringBuilder result = new StringBuilder();
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            try (BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
                 for (String line; (line = reader.readLine()) != null; ) {
                     result.append(line);
                 }
             }
             String s = result.toString();
-            System.out.println(s);*/
-            return "maybe";
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        } catch (ProtocolException e) {
-            throw new RuntimeException(e);
+            log.info("Scaling up returned: " + s);
+            cache.setLocalNodeRunning(ln, true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -83,19 +72,19 @@ public class ScalerClient {
 
 
     public void down(LocalNode ln) throws Exception {
-        arnold.shutdown(ln.getSpec().getUsername(),ln.getSpec().getPassword(), ln.getSpec().getIpAddress(), 22);
+        arnold.shutdown(ln.getSpec().getUsername(), ln.getSpec().getPassword(), ln.getSpec().getIpAddress(), 22);
         cache.setLocalNodeRunning(ln, false);
     }
 
-    public class ParameterStringBuilder {
+    public static class ParameterStringBuilder {
         public static String getParamsString(Map<String, String> params)
                 throws UnsupportedEncodingException {
             StringBuilder result = new StringBuilder();
 
             for (Map.Entry<String, String> entry : params.entrySet()) {
-                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                result.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
                 result.append("=");
-                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+                result.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
                 result.append("&");
             }
 
